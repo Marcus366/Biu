3e6e0a248c7dbca4e91fb34a4346a00a82bd512b
N <n.zhangzq@gmail.com>
2014-12-24 22:07:16 +0800

    biu

--- gen/com/ajaybe/biu/R.java
+++ gen/com/ajaybe/biu/R.java
@@ -22,14 +22,14 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int background=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
-        public static final int button1=0x7f080003;
+        public static final int action_settings=0x7f080003;
+        public static final int button1=0x7f080002;
         public static final int password=0x7f080001;
-        public static final int textView1=0x7f080000;
-        public static final int username=0x7f080002;
+        public static final int username=0x7f080000;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
---
+++ src/org/ajaybe/biu/LoginActivity.java
@@ -0,0 +1,105 @@
+package com.ajaybe.biu;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Iterator;
+
+import org.apache.http.Header;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    
+    public void Login(View view) {
+    	EditText edittext_username = (EditText) findViewById(R.id.username);
+    	EditText edittext_password = (EditText) findViewById(R.id.password);
+    	String username = edittext_username.getText().toString();
+    	String password = edittext_password.getText().toString();
+    	RequestParams params = new RequestParams();
+    	params.put("username", username);
+    	params.put("password", password);
+    	
+    	AsyncHttpClient client = new AsyncHttpClient();
+    	client.post("http://106.187.100.252/login", params, new AsyncHttpResponseHandler() {
+
+			@Override
+			public void onSuccess(int statusCode, Header[] headers,
+					byte[] responseBody) {
+				Log.e("LOG", "onSuccess(int, Header[], JSONObject) callback was received");
+				if (statusCode == 200) {
+					try {
+						JSONObject jsonObject = new JSONObject(new String(responseBody, "gb2312"));
+						Iterator<String> jsonIter = jsonObject.keys();
+						while (jsonIter.hasNext()) {
+							String key = jsonIter.next();
+							if (key.equals("code")) {
+								int response = jsonObject.getInt(key);
+								if (response == 0) {
+									Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+									LoginActivity.this.startActivity(intent);
+									LoginActivity.this.finish();
+								} else if (response == 1) {
+									Toast.makeText(LoginActivity.this, "用户名不存在", Toast.LENGTH_LONG).show();
+								} else {
+									Toast.makeText(LoginActivity.this, "密码错误", Toast.LENGTH_LONG).show();
+								}
+							}
+						}
+					} catch (JSONException e) {
+						
+					} catch (UnsupportedEncodingException e) {
+						
+					}
+				}
+			}
+
+			@Override
+			public void onFailure(int statusCode, Header[] headers,
+					byte[] responseBody, Throwable error) {
+				Log.w("LOG", "onFailure(int, Header[], Throwable, JSONObject) callback was received");
+				Toast.makeText(LoginActivity.this, "联网错误", Toast.LENGTH_LONG).show();
+			}
+    		
+    	});
+    }
+}
